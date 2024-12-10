a4bb025ac9d42839fd0d92495ecb164b5e48d9d5
the-castaway <jccd.designs@gmail.com>
2024-12-10 12:29:46 -0800

    [PROJECTS] Layout modifications

--- src/app/projects/layout.tsx
+++ src/app/projects/layout.tsx
@@ -5,15 +5,16 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     // Convert children into an array
     const childrenArray = React.Children.toArray(children);
 
+
     return (
         <div style={{ display: 'flex' }}>
-            <div style={{ flex: 1, padding: '10px', background: '#f8f9fa' }}>
+            <div style={{ flex: 1, padding: '10px', background: '#1e1e1e' }}>
                 {childrenArray[0] || "Default Column 1"}
             </div>
-            <div style={{ flex: 1, padding: '10px', background: '#e9ecef' }}>
+            <div style={{ flex: 1, padding: '10px', background: '#1e1e1e' }}>
                 {childrenArray[1] || "Default Column 2"}
             </div>
-            <div style={{ flex: 1, padding: '10px', background: '#dee2e6' }}>
+            <div style={{ flex: 1, padding: '10px', background: '#1e1e1e' }}>
                 {childrenArray[2] || "Default Column 3"}
             </div>
         </div>
