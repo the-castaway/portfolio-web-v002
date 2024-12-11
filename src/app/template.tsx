55dc3447dc44e186ebf815eb93d4fdc60a23d74b
the-castaway <jccd.designs@gmail.com>
2024-12-10 22:40:39 -0800

    [TRANSITION] Scaffolding

--- src/app/template.tsx
+++ src/app/template.tsx
@@ -11,15 +11,69 @@ export default function Template({ children }: { children: React.ReactNode }) {
         <div>
             <div id="mask"
                 style={{
-                    border: 'solid 1px black',
-                    height: '100px',
-                    width: '100px',
+                    height: '100vh',
+                    width: '100vw',
                     position: 'fixed',
-                    top: '50%',
-                    background: "#dddd"
+                    top: '0%',
+                    left: '0%',
+                    background: "rgba(255, 255, 255, 0.4)",
+                    transform: "rotate(0)",
+                    pointerEvents: 'none',
+                    zIndex: '100',
+                }}>
+                <div id="mask-bottom"
+                    style={{
+                        height: '100vh',
+                        width: '100vw',
+                        position: 'absolute',
+                        bottom: '0%',
+                        left: '0%',
+                        transform: 'translate(0%, 100%)',
+                        background: "#000000",
+                        border: "dashed red",
+                    }} />
+                <div id="mask-top"
+                    style={{
+                        height: '100vh',
+                        width: '100vw',
+                        position: 'absolute',
+                        top: '0%',
+                        left: '0%',
+                        transform: 'translate(0%, -100%)',
+                        background: "#000000",
+                        border: "dashed red",
+                    }} />
+                <div id="mask-left"
+                    style={{
+                        height: '100vh',
+                        width: '100vw',
+                        position: 'absolute',
+                        top: '0%',
+                        left: '0%',
+                        transform: 'translate(-100%, 0%)',
+                        background: "#000000",
+                        border: "dashed red",
+                    }} />
+                <div id="mask-right"
+                    style={{
+                        height: '100vh',
+                        width: '100vw',
+                        position: 'absolute',
+                        top: '0%',
+                        right: '0%',
+                        transform: 'translate(100%, 0%)',
+                        background: "#000000",
+                        border: "dashed red",
+                    }} />
+            </div>
+            <div id="mask-content"
+                style={{
+                    position: 'relative',
+                    zIndex: '0',
+                }}>
+                {children}
+            </div>
 
-                }} />
-            {children}
         </div>
     )
 }
--- src/app/utils/transition.tsx
+++ src/app/utils/transition.tsx
@@ -2,35 +2,80 @@ import gsap from "gsap";
 import { AppRouterInstance } from "next/dist/shared/lib/app-router-context.shared-runtime";
 
 export const transitionPageIn = () => {
-    const mask = document.getElementById("mask")
+    const mask = document.getElementById("mask");
+    const maskTop = document.getElementById("mask-top");
+    const maskBottom = document.getElementById("mask-bottom");
+    const maskLeft = document.getElementById("mask-left");
+    const maskRight = document.getElementById("mask-right");
     const transitionPageInTL = gsap.timeline();
     transitionPageInTL.set(
         mask,
         {
-            y: 0,
+            rotate: 180,
         })
-        .to(
-            mask,
+        .set(
+            maskBottom,
             {
-                y: 100,
-                duration: 2
+                bottom: "50%",
             })
+        .set(
+            maskTop,
+            {
+                top: "50%",
+            })
+        .set(
+            maskLeft,
+            {
+                left: "50%",
+            })
+        .set(
+            maskRight,
+            {
+                right: "50%",
+            })
+        .to(mask, { rotate: 0, duration: 1 }, 0)
+        .to(maskBottom, { bottom: 0, duration: 1 }, 0)
+        .to(maskTop, { top: 0, duration: 1 }, 0)
+        .to(maskLeft, { left: 0, duration: 1 }, 0)
+        .to(maskRight, { right: 0, duration: 1 }, 0)
 }
 
 export const transitionPageOut = (href: string, router: AppRouterInstance) => {
-    const mask = document.getElementById("mask")
+    const mask = document.getElementById("mask");
+    const maskTop = document.getElementById("mask-top");
+    const maskBottom = document.getElementById("mask-bottom");
+    const maskLeft = document.getElementById("mask-left");
+    const maskRight = document.getElementById("mask-right");
     const transitionPageOutTL = gsap.timeline();
     transitionPageOutTL.set(
         mask,
         {
-            y: 100,
+            y: 0,
         })
-        .to(
-            mask,
+        .set(
+            maskBottom,
             {
-                y: 0,
-                duration: 2,
-                onComplete: () => { router.push(href) }
+                bottom: 0,
             })
+        .set(
+            maskTop,
+            {
+                top: 0,
+            })
+        .set(
+            maskLeft,
+            {
+                left: 0,
+            })
+        .set(
+            maskRight,
+            {
+                right: 0,
+            })
+        .to(mask, { rotate: 180, duration: 1, onComplete: () => { router.push(href) } }, 0)
+        .to(maskBottom, { bottom: "50%", duration: 1 }, 0)
+        .to(maskTop, { top: "50%", duration: 1 }, 0)
+        .to(maskLeft, { left: "50%", duration: 1 }, 0)
+        .to(maskRight, { right: "50%", duration: 1 }, 0)
 
 }
