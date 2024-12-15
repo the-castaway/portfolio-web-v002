b8cb430eef77c140e15d1e08f5fb6950f6499bc0
the-castaway <jccd.designs@gmail.com>
2024-12-15 00:06:14 -0800

    [HOME] 3D model scaffolding

--- src/app/components/scene/logoModel.js
+++ src/app/components/scene/logoModel.js
@@ -1,15 +1,25 @@
 import React, { useEffect, useRef } from 'react'
-import { MeshTransmissionMaterial, useGLTF, Text, SpotLight } from "@react-three/drei";
+import { MeshTransmissionMaterial, useGLTF, Text, Environment, PerspectiveCamera } from "@react-three/drei";
+import { EffectComposer, DepthOfField } from '@react-three/postprocessing';
 import { useFrame, useThree } from '@react-three/fiber'
 import { useControls } from 'leva'
+import { DirectionalLight } from 'three';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
 
 export default function LogoModel() {
     const { nodes } = useGLTF("/media/3D/jc_logo.glb");
     const { viewport } = useThree()
     const logo = useRef(null);
+    const logoLeft = useRef(null);
+    const logoRight = useRef(null);
+    gsap.registerPlugin(ScrollTrigger)
+    const trigger = document.getElementById("trigger");
+
     useFrame(() => {
-        logo.current.rotation.x += 0.002
-        logo.current.rotation.y += 0.002
+        //logo.current.rotation.x += 0.002
+        logo.current.rotation.z -= 0.006
     })
     const materialProps = useControls({
         thickness: { value: 0.2, min: 0, max: 3, step: 0.05 },
@@ -20,24 +30,70 @@ export default function LogoModel() {
         backside: { value: true },
     })
 
+    const postProcessingProps = useControls({
+        focusDistance: { value: 0.1, min: 0, max: 3, step: 0.01 },
+        focalLength: { value: 0.1, min: 0, max: 3, step: 0.01 },
+        bokehScale: { value: 1, min: 0, max: 10, step: 0.5 },
+        height: { value: 480 },
+    })
+
     useEffect(() => {
-        console.log(nodes)
+        console.log(trigger)
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: trigger,
+                pin: false,
+                start: 'top bottom',
+                end: '+2000',
+                scrub: 1,
+                markers: true,
+            }
+        });
+        tl.to(logo.current.scale, {
+            x: 3,
+            y: 3,
+            z: 3,
+        }, 0)
+        tl.to(logoRight.current.rotation, {
+            z: 10,
+        }, 0)
+        tl.to(logoRight.current.position, {
+            x: 2,
+        }, 0)
+        tl.to(logoLeft.current.rotation, {
+            z: 10,
+        }, 0)
+        tl.to(logoLeft.current.position, {
+            x: -2,
+        }, 0)
+
     }, [])
     return (
-        <group scale={viewport.width / 3.75}>
-            <Text position={[0, 0, -2]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
-                hello world!
-            </Text>
-            <ambientLight />
-            <group ref={logo} position={[0, 0, -1]} scale={[0.6, 0.6, 0.6]}>
-                <mesh {...nodes.logo_left}>
-                    <MeshTransmissionMaterial {...materialProps} />
-                </mesh>
-                <mesh {...nodes.logo_right}>
-                    <MeshTransmissionMaterial {...materialProps} />
-                </mesh>
-            </group>
-
-        </group >
+        <>
+            <PerspectiveCamera
+                makeDefault
+                fov={50}
+                position={[0, 0, 100]}
+            />
+            <group scale={viewport.width / 3.75}>
+                <Text position={[0, 0, -2]} fontSize={0.5} color="white" anchorX="center" anchorY="middle">
+                    DESIGNER + DEVELOPER + DESIGNER + DEVELOPER
+                </Text>
+                <ambientLight intensity={1} />
+                <group ref={logo} position={[0, 0, -1]} scale={[0.6, 0.6, 0.6]} rotation={[1.3, 0, 0]}>
+                    <mesh ref={logoLeft} {...nodes.logo_left}>
+                        <MeshTransmissionMaterial {...materialProps} />
+                    </mesh>
+                    <mesh ref={logoRight} {...nodes.logo_right}>
+                        <MeshTransmissionMaterial {...materialProps} />
+                    </mesh>
+                </group>
+                <Environment preset='sunset' />
+                <EffectComposer>
+                    <DepthOfField {...postProcessingProps}        // Resolution of the effect
+                    />
+                </EffectComposer>
+            </group >
+        </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,87 +1,31 @@
 // `app/home.tsx` is the UI for the `/` URL
 "use client"
-import Image from "next/image";
-import styles from "./styles/page.module.css";
-
 import { useEffect, useRef } from "react";
-import { useRouter } from 'next/navigation'
-import dynamic from 'next/dynamic'
+// Styles
+import styles from "./styles/page.module.css";
+// Components
+import Image from "next/image";
 import { Canvas } from "@react-three/fiber";
 import LogoModel from "./components/scene/logoModel";
-import { Environment } from '@react-three/drei'
 
 
 export default function Home() {
-
-  // Canvas reference
-  const canvasRef = useRef<HTMLCanvasElement>(null)
-
-  // Create imgRef reference
-  const imgRef = useRef<HTMLImageElement>(null)
-
-  // Effect when image and context are loaded
-  useEffect(() => {
-    const canvas = canvasRef.current
-
-    if (canvas === null)
-      return
-
-    const context = canvas.getContext('2d')
-
-    if (context === null)
-      return
-
-    // Get the `img` from reference
-    const image = imgRef.current
-
-    if (image === null)
-      return
-
-    // Draw the image to the context
-    context.drawImage(image, 0, 0)
-  }, [imgRef])
-
-  const route = useRouter()
   return (
 
     <div>
-      <main className={styles.main}>
+      <main className={styles.main} style={{ position: 'relative' }}>
 
-        <Canvas style={{ background: '#0E0E10' }}>
+        <Canvas style={{ height: '100vh', position: 'fixed', top: '0' }}>
           <LogoModel />
-          <Environment preset='night' />
         </Canvas>
-
-        <canvas
-          className='col-start-15 col-span-10'
-          ref={canvasRef}
-          style={{
-            border: 'solid 1px black',
-          }}
-        ></canvas>
-        <Image
-          // attach a reference
-          ref={imgRef}
-          // source to the image
-          src={"/media/next.svg"}
-          width={180}
-          height={38}
-          // Or import in a next.js fashion
-          // import myImage from './myimage.png'
-          // src={myImage}
-          alt=""
-          priority={true}
-        // Hide it from displaying
-        // style={{
-        //   display: 'none',
-        // }}
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+        <div id="trigger" style={{ marginTop: '100vh' }}>
+          <ol>
+            <li>
+              Get started by editing <code>src/app/page.tsx</code>.
+            </li>
+            <li>Save and see your changes instantly.</li>
+          </ol>
+        </div>
       </main>
     </div>
   );
