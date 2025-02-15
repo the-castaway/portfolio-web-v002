6fb67efdb7c166e993eafd18cec709fff3485c06
the-castaway <jccd.designs@gmail.com>
2025-02-14 17:20:54 -0800

    [NAV] Link on logo and styling updates

--- src/app/components/nav.tsx
+++ src/app/components/nav.tsx
@@ -5,6 +5,7 @@ import gsap from "gsap";
 import styles from "../styles/nav.module.css";
 // Components
 import NavLink from "./navLink"
+import NavLogo from "./navLogo";
 // Context
 import { useScreenSize } from "../context/screenSizeContext";
 
@@ -45,7 +46,7 @@ const Nav = () => {
         setCollapsed(false);
         const expandNavTL = gsap.timeline();
         // Ensure isMobile is up-to-date
-        const navButtonWidth = isMobile ? "calc(100vw - 20px)" : "60vw";
+        const navButtonWidth = isMobile ? "calc(100vw - 20px)" : "max(35vw, 350px)";
         expandNavTL
             .to(navOverlay.current,
                 {
@@ -140,30 +141,25 @@ const Nav = () => {
     }
 
     return (
-        <nav className={`${styles.nav}`}>
-            <div className={`${styles.navIcon}`}>
-                <svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M1.56452 25.4999L9.38842 17.6872C11.9763 15.1019 11.9763 10.8968 9.38842 8.31223L1.56452 0.499878L2.00272e-05 2.06214L7.82392 9.87483C9.54837 11.5979 9.54837 14.4019 7.82392 16.1249L2.00272e-05 23.9376L1.56452 25.4999Z" className={`${styles.navIconSVG}`} />
-                    <path d="M26.4355 25.4999L28 23.9376L20.1768 16.125C18.4513 14.4019 18.4513 11.5979 20.1768 9.87487L28 2.06287L26.4355 0.50061L18.6123 8.31261C16.024 10.8968 16.024 15.1023 18.6123 17.6876L26.4355 25.5003V25.4999Z" className={`${styles.navIconSVG}`} />
-                </svg>
-            </div>
+        <nav className={styles.nav}>
+            <NavLogo />
             <div className={`${styles.navDetails} grid`}>
-                <div className={`${styles.navTitleContainer}`}>
+                <div className={styles.navTitleContainer}>
                     <p className={`detail`}>Product&nbsp;Designer<br /><span className={`textColorGrey`}>+ Developer</span></p>
                 </div>
-                <div className={`${styles.navCurrentlyContainer}`}>
+                <div className={styles.navCurrentlyContainer}>
                     <p className={`detail`}>
                         Currently<br /><span className={`textColorGrey`}>@Meta</span>
                     </p>
                 </div>
-                <div className={`${styles.navGeoContainer}`}>
+                <div className={styles.navGeoContainer}>
                     <p className={`detail`}>
                         SF,&nbsp;Bay&nbsp;Area<br /><span className={`textColorGrey`}>{currentTime}</span>
                     </p>
                 </div>
             </div>
             <div ref={navOverlay} className={styles.navOverlay} onClick={() => { collapseNav(); }} />
-            <div ref={navButton} className={`${styles.navButton}`}>
+            <div ref={navButton} className={styles.navButton}>
                 <div ref={navButtonBorder} className={styles.navButtonBorder} />
                 <div ref={navButtonIcon} className={styles.navButtonIcon} onClick={() => { toggleNav(); }}>
                     <div className={styles.navButtonIconVertical} />
@@ -176,14 +172,14 @@ const Nav = () => {
                 <div ref={navModal} className={styles.navModal}>
                     <div className={styles.navModalContent}>
                         <div className={styles.navModalContentDirectory}>
-                            <div className={`${styles.navModalContentHeader}`}>
+                            <div className={styles.navModalContentHeader}>
                                 <p className={`detail textColorGrey`}>
                                     <span className={`textColorGrey`}>
                                         Directory
                                     </span>
                                 </p>
                             </div>
-                            <div className={`${styles.navModalContentDirectoryList}`} onClick={() => { collapseNav(); }} >
+                            <div className={styles.navModalContentDirectoryList} onClick={() => { collapseNav(); }} >
                                 <NavLink href="/" label="Work" number="[ 01 ]" />
                                 <NavLink href="/archive" label="About" number="[ 02 ]" />
                                 <NavLink href="/about" label="Lab" number="[ 03 ]" />
@@ -191,7 +187,7 @@ const Nav = () => {
                             </div>
                         </div>
                         <div className={styles.navModalContentContact}>
-                            <div className={`${styles.navModalContentHeader}`}>
+                            <div className={styles.navModalContentHeader}>
                                 <p className={`detail textColorGrey`}>
                                     <span className={`textColorGrey`}>
                                         Contact
---
+++ src/app/components/navLogo.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { usePathname, useRouter } from "next/navigation"
+import { transitionPageOut } from "../utils/transition/transition"
+import styles from "../styles/navLogo.module.css"
+
+
+const NavLogo = () => {
+    const router = useRouter();
+    const pathname = usePathname();
+    const handleClick = () => {
+        if (pathname !== '/') {
+            transitionPageOut('/', router)
+        }
+    }
+
+    return (
+        <a className={styles.navLogo} onClick={handleClick}>
+            <svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M1.56452 25.4999L9.38842 17.6872C11.9763 15.1019 11.9763 10.8968 9.38842 8.31223L1.56452 0.499878L2.00272e-05 2.06214L7.82392 9.87483C9.54837 11.5979 9.54837 14.4019 7.82392 16.1249L2.00272e-05 23.9376L1.56452 25.4999Z" className={styles.navLogoSVG} />
+                <path d="M26.4355 25.4999L28 23.9376L20.1768 16.125C18.4513 14.4019 18.4513 11.5979 20.1768 9.87487L28 2.06287L26.4355 0.50061L18.6123 8.31261C16.024 10.8968 16.024 15.1023 18.6123 17.6876L26.4355 25.5003V25.4999Z" className={styles.navLogoSVG} />
+            </svg>
+        </a >
+    )
+}
+
+export default NavLogo
